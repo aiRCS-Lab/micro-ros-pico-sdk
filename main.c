c8c415ab78e8765f56147a7c6fbe3b9af15e04c5
Julien Geneste <julien@aircslab.fr>
2023-05-05 18:42:07 +0200

    fixed transport methode

--- main.c
+++ main.c
@@ -10,20 +10,34 @@
 #include "pico/stdlib.h"
 #include "pico_uart_transports.h"
 
-const uint LED_PIN = 25;
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc); return 1;}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+#define LED_PIN 25
 
 rcl_publisher_t publisher;
-std_msgs__msg__Int32 msg;
+rcl_subscription_t subscriber;
+std_msgs__msg__Int32 send_msg;
+std_msgs__msg__Int32 recv_msg;
 
-void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 {
-    rcl_ret_t ret = rcl_publish(&publisher, &msg, NULL);
-    msg.data++;
+	(void) last_call_time;
+	if (timer != NULL) {
+		RCSOFTCHECK(rcl_publish(&publisher, &send_msg, NULL));
+		printf("Sent: %d\n", send_msg.data);
+		send_msg.data++;
+	}
 }
 
-int main()
+void subscription_callback(const void * msgin)
 {
-    rmw_uros_set_custom_transport(
+	const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+	printf("Received: %d\n", msg->data);
+}
+
+int main(int argc, const char * const * argv)
+{
+	rmw_uros_set_custom_transport(
 		true,
 		NULL,
 		pico_serial_transport_open,
@@ -32,53 +46,55 @@ int main()
 		pico_serial_transport_read
 	);
 
-    gpio_init(LED_PIN);
+	gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    rcl_timer_t timer;
-    rcl_node_t node;
-    rcl_allocator_t allocator;
-    rclc_support_t support;
-    rclc_executor_t executor;
+  	rcl_allocator_t allocator = rcl_get_default_allocator();
+	rclc_support_t support;
 
-    allocator = rcl_get_default_allocator();
+	// create init_options
+	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
-    // Wait for agent successful ping for 2 minutes.
-    const int timeout_ms = 1000; 
-    const uint8_t attempts = 120;
+	// create node
+	rcl_node_t node;
+	RCCHECK(rclc_node_init_default(&node, "int32_publisher_subscriber_rclc", "", &support));
 
-    rcl_ret_t ret = rmw_uros_ping_agent(timeout_ms, attempts);
+	// create publisher
+	RCCHECK(rclc_publisher_init_default(
+		&publisher,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		"int32_publisher"));
 
-    if (ret != RCL_RET_OK)
-    {
-        // Unreachable agent, exiting program.
-        return ret;
-    }
+  	// create subscriber
+	RCCHECK(rclc_subscription_init_default(
+		&subscriber,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		"int32_subscriber"));
 
-    rclc_support_init(&support, 0, NULL, &allocator);
+	// create timer,
+	rcl_timer_t timer;
+	const unsigned int timer_timeout = 1000;
+	RCCHECK(rclc_timer_init_default(
+		&timer,
+		&support,
+		RCL_MS_TO_NS(timer_timeout),
+		timer_callback));
 
-    rclc_node_init_default(&node, "pico_node", "", &support);
-    rclc_publisher_init_default(
-        &publisher,
-        &node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-        "pico_publisher");
+	// create executor
+	rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
+	RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+	RCCHECK(rclc_executor_add_timer(&executor, &timer));
+	RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &recv_msg, &subscription_callback, ON_NEW_DATA));
 
-    rclc_timer_init_default(
-        &timer,
-        &support,
-        RCL_MS_TO_NS(1000),
-        timer_callback);
+	send_msg.data = 0;
 
-    rclc_executor_init(&executor, &support.context, 1, &allocator);
-    rclc_executor_add_timer(&executor, &timer);
+	gpio_put(LED_PIN, 1);
+	
+  	rclc_executor_spin(&executor);
 
-    gpio_put(LED_PIN, 1);
-
-    msg.data = 0;
-    while (true)
-    {
-        rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
-    }
-    return 0;
+	RCCHECK(rcl_subscription_fini(&subscriber, &node));
+	RCCHECK(rcl_publisher_fini(&publisher, &node));
+	RCCHECK(rcl_node_fini(&node));
 }
